61bea4e4670f4a76dbb898ef8732dd49ad3a0202
prathamesh <prathamesh.math@gmail.com>
2024-08-21 20:04:11 +0530

    Change grid layout
        Side menu is fixed, might add hide/view featur later

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import "./style.css"
 import { Project } from "./scripts/projectHandler.js";
 
 const projectMenuBtn = document.querySelector('#project-menu-btn');
-
-projectMenuBtn.addEventListener("click", ()=>
+const projectMenu  = document.querySelector('#side-column');
+const mainArea = document.querySelector("#main-area");
+projectMenuBtn.addEventListener("click", (e)=>
 {
+    e.stopPropagation();
 
+    // projectMenu.style.width   = '0px';
+    // projectMenu.style.position = "absolute";
+    // projectMenu.style.gridColumn = '1/-1';
+    // mainArea.style.gridColumn = '1/5';
+    // projectMenu.style.left = "-100%";
 })
