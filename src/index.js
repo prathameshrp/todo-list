2d19143c645bec7b987e2de64fd0d70a9621d801
prathamesh <prathamesh.math@gmail.com>
2024-08-22 19:16:25 +0530

    Update index.js
        Populates side menu with default project names

--- src/index.js
+++ src/index.js
@@ -25,9 +25,24 @@ class ProjectManager
 
 function populateDOM()
 {
-    
+    const content = generateFakeContent();
+    const projects = content.projectNames;
+    const lists = content.projectLists;
+    const tasks = content.projectTasks;
+    for(let i = 0; i < projects.length; ++i)
+    {
+        const projecBar = document.querySelector("#all-projects");
+        const addProjectBtn = document.querySelector("#add-project");
+        const pli = document.createElement('li');
+        const pbtn = document.createElement('button');
+        pbtn.textContent = projects[i];
+        pli.setAttribute('project-index', i);
+        pli.appendChild(pbtn);
+        projecBar.insertBefore(pli, addProjectBtn);
+    }
 }
 
+populateDOM();
 
 function generateFakeContent()
 {
@@ -36,6 +51,8 @@ function generateFakeContent()
     const projectLists = [["Productivity", "Exercise", "Dailies"], 
                           ["Today", "Tomorrow", "Weekly Todos'"]];
 
-    const tasks = [[[["Create To Dos", "Easy to create"], "Manage To Dos", ["Set Descriptions", "Like This, Lorem Ipsum? Hyderate yoursel"]], ["Drink Water", "Run for 30mins"], ["Buy Milk", "Cook breakfast"]], 
+    const projectTasks = [[[["Create To Dos", "Easy to create"], "Manage To Dos", ["Set Descriptions", "Like This, Lorem Ipsum? Hyderate yoursel"]], ["Drink Water", "Run for 30mins"], ["Buy Milk", "Cook breakfast"]], 
     [["Study Biology", "Lie on floor", "Think about life"], ["Attend lectures", "Buy something sweet"], ["Enjoy Holidays"]]];
+
+    return {projectNames, projectLists, projectTasks};
 }
