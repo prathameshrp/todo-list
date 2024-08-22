f12bbf7484aa4118bac57c1a9d4fd25e6903e6df
prathamesh <prathamesh.math@gmail.com>
2024-08-22 19:06:26 +0530

    Add dummy text arrays

--- src/index.js
+++ src/index.js
@@ -27,3 +27,15 @@ function populateDOM()
 {
     
 }
+
+
+function generateFakeContent()
+{
+    const projectNames = ["Neo.do", "Default"];
+
+    const projectLists = [["Productivity", "Exercise", "Dailies"], 
+                          ["Today", "Tomorrow", "Weekly Todos'"]];
+
+    const tasks = [[[["Create To Dos", "Easy to create"], "Manage To Dos", ["Set Descriptions", "Like This, Lorem Ipsum? Hyderate yoursel"]], ["Drink Water", "Run for 30mins"], ["Buy Milk", "Cook breakfast"]], 
+    [["Study Biology", "Lie on floor", "Think about life"], ["Attend lectures", "Buy something sweet"], ["Enjoy Holidays"]]];
+}
