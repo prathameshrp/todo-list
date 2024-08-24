2e9a6875b226bba4a05b1f40cca378805ce733c6
prathamesh <prathamesh.math@gmail.com>
2024-08-24 13:48:58 +0530

    Move populateNavigatorList() function to index
        Uses the newPopulate function from dom to populate stuff now

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import "../assets/fonts/stylesheet.css"
 import "./style.css"
 import { Project } from "./scripts/projectHandler.js";
 import { ToDoList } from "./scripts/ListHandler.js";
-import placeholderContent, {populateDOM, populateNavigatorList, populateListTask, populateNewProject, populateNewList, populateNewtask} from "./scripts/populateDOM.js";
+import placeholderContent, {populateListTask, populateNewProject, populateNewList, populateNewtask} from "./scripts/populateDOM.js";
 import { ToDoTask } from "./scripts/taskHandler.js";
 
+let active_project = 0;
+let active_list = 0;
 class ProjectManager
 {
     static #projectsInStorage = [];
@@ -56,138 +58,184 @@ class ProjectManager
     // console.log(ProjectManager.getAllProjects());  
 })();
 
+function populateDOM(projects)
+{
+    for(let i = 0; i < projects.length; ++i)
+    {
+     populateNewProject(projects[i], i);
+         // pli.addEventListener("click", ()=>
+    //     {populateNavigatorList(project.getLists(), index)})
+    }
+    // populateNavigatorList(projects[0].getLists());
+}
 populateDOM(ProjectManager.getAllProjects());
+
+function addEventsToProject(projects)
+{
+    for(let i = 0; i < projects.length; ++i)
+    {
+        const projecBtn = document.querySelector(`[project-index='${i}']`);
+        projecBtn.addEventListener("click", (e)=>{
+            e.stopPropagation();
+            populateNavigatorList(projects[i].getLists(), i);
+        });
+    }
+}
+addEventsToProject(ProjectManager.getAllProjects());
+
+function populateNavigatorList(lists, projectIndex)
+{    
+   
+    const listNavigator = document.querySelector("#all-lists");
+    const addListBtn = document.querySelector("#add-list");
+    listNavigator.replaceChildren(addListBtn);
+    addListBtn.setAttribute("active-project", projectIndex);
+    for(let i = 0; i<lists.length; ++i)
+        {
+            populateNewList(i, lists[i])
+
+        }
+    if(lists.length !=0)
+            {
+    populateListTask(0, lists[0].getAllTasks());
+            }
+    else
+    {
+        return;
+    }
+}
+
 
 // placeholderContent(ProjectManager);
 
-const runApp = (function (doc){
-    const addProjectBtn = doc.querySelector("#add-project");
+// const runApp = (function (doc){
+//     const addProjectBtn = doc.querySelector("#add-project");
 
-    addProjectBtn.addEventListener("click", (e)=>{
-        e.stopPropagation();
-        openProjectModal();
-    })
+//     addProjectBtn.addEventListener("click", (e)=>{
+//         e.stopPropagation();
+//         openProjectModal();
+//     })
 
-    const addListButton = doc.querySelector("#add-list");
-    addListButton.addEventListener("click", (e)=>{
-        e.stopPropagation();
-        openListModal();
-    })
+//     const addListButton = doc.querySelector("#add-list");
+//     addListButton.addEventListener("click", (e)=>{
+//         e.stopPropagation();
+//         openListModal();
+//     })
 
 
-    const addTaskButton = doc.querySelector("#add-task");
-    addTaskButton.addEventListener("click", (e)=>{
-        e.stopPropagation();
-        openTaskModal();
-    })
+//     const addTaskButton = doc.querySelector("#add-task");
+//     addTaskButton.addEventListener("click", (e)=>{
+//         e.stopPropagation();
+//         openTaskModal();
+//     })
 
-    const submitPBtn = document.querySelector("#submit-project");
-    submitPBtn.addEventListener("click", (e)=>
-    {
-    e.stopPropagation();
-    // add project to backend logic:
-    addNewProject();
-    })
+//     const submitPBtn = document.querySelector("#submit-project");
+//     submitPBtn.addEventListener("click", (e)=>
+//     {
+//     e.stopPropagation();
+//     // add project to backend logic:
+//     addNewProject();
+//     })
 
-    const submitLBtn = document.querySelector("#submit-list");
-    submitLBtn.addEventListener("click", (e)=>
-    {
-        e.stopPropagation();
-    // add list to backend logic:
+//     const submitLBtn = document.querySelector("#submit-list");
+//     submitLBtn.addEventListener("click", (e)=>
+//     {
+//         e.stopPropagation();
+//     // add list to backend logic:
     
-    addNewList();
-    });
+//     addNewList();
+//     });
 
-    const submitTBtn = document.querySelector("#submit-task");
-    submitTBtn.addEventListener("click", (e)=>
-    {
-        e.stopPropagation();
-    // add task to backend logic:
+//     const submitTBtn = document.querySelector("#submit-task");
+//     submitTBtn.addEventListener("click", (e)=>
+//     {
+//         e.stopPropagation();
+//     // add task to backend logic:
     
-    addNewTask();
-    });
+//     addNewTask();
+//     });
 
-})(document);
+// })(document);
 
 
-function openProjectModal()
-{
-    const projectDialog = document.querySelector("#project-dialog");
-    projectDialog.showModal();
+// function openProjectModal()
+// {
+//     const projectDialog = document.querySelector("#project-dialog");
+//     projectDialog.showModal();
 
-}
+// }
 
-function addNewProject()
-{
+// function addNewProject()
+// {
 
-//add to array
-    const projecNameEle = document.querySelector("#project-title");
-    const newProjectName = projecNameEle.value;
-    const newProject = new Project(newProjectName);
+// //add to array
+//     const projecNameEle = document.querySelector("#project-title");
+//     const newProjectName = projecNameEle.value;
+//     const newProject = new Project(newProjectName);
     
-    ProjectManager.addProject(newProject);
-    // populateDOM(ProjectManager.getAllProjects());
-    // populateNavigatorList(newProject.getLists());
-    populateNewProject(newProject, ProjectManager.getAllProjects().length-1);
-    const projectDialog = document.querySelector("#project-dialog");
-    projectDialog.close();
-}
+//     ProjectManager.addProject(newProject);
+//     // populateDOM(ProjectManager.getAllProjects());
+//     // populateNavigatorList(newProject.getLists());
+//     populateNewProject(newProject, ProjectManager.getAllProjects().length-1);
+//     const projectDialog = document.querySelector("#project-dialog");
+//     projectDialog.close();
+// }
 
-function openListModal()
-{
-    const listDialog = document.querySelector("#list-dialog");
-    listDialog.showModal();
+// function openListModal()
+// {
+//     const listDialog = document.querySelector("#list-dialog");
+//     listDialog.showModal();
     
-}
+// }
 
-function addNewList()
-{
+// function addNewList()
+// {
 
-//add to array
-    const activeProject = document.querySelector("#add-list").getAttribute("active-project");
-    const listNameEle = document.querySelector("#list-title");
-    const newListName = listNameEle.value;
-    const newList = new ToDoList(newListName);
-    const listParentProject = ProjectManager.getAllProjects()[activeProject];
-    console.log("This Must be all projects", ProjectManager.getAllProjects());
-    console.log("This Must be index I am looking for", activeProject);
-
-
-    console.log("This Must be project", listParentProject);
-    listParentProject.createList(newList);
-    // populateDOM(ProjectManager.getAllProjects());
-    // populateNavigatorList(newProject.getLists());
-    populateNewList(listParentProject.getLists().length -1, newList);
-    const listDialog = document.querySelector("#list-dialog");
-    listDialog.close();
-}
+// //add to array
+//     const activeProject = document.querySelector("#add-list").getAttribute("active-project");
+//     const listNameEle = document.querySelector("#list-title");
+//     const newListName = listNameEle.value;
+//     const newList = new ToDoList(newListName);
+//     const listParentProject = ProjectManager.getAllProjects()[activeProject];
+//     console.log("This Must be all projects", ProjectManager.getAllProjects());
+//     console.log("This Must be index I am looking for", activeProject);
 
 
+//     console.log("This Must be project", listParentProject);
+//     listParentProject.createList(newList);
+//     // populateDOM(ProjectManager.getAllProjects());
+//     // populateNavigatorList(newProject.getLists());
+//     populateNewList(listParentProject.getLists().length -1, newList);
+//     const listDialog = document.querySelector("#list-dialog");
+//     listDialog.close();
+// }
 
-function openTaskModal()
-{
-    const taskDialog = document.querySelector("#task-dialog");
-    taskDialog.showModal();
+
+
+// function openTaskModal()
+// {
+//     const taskDialog = document.querySelector("#task-dialog");
+//     taskDialog.showModal();
     
-}
+// }
 
-function addNewTask()
-{
+// function addNewTask()
+// {
 
-//add to array
-    const activeProject = document.querySelector("#add-list").getAttribute("active-project");
-    const activeList = document.querySelector("#add-task").getAttribute("active-list");
-    const taskNameEle = document.querySelector("#task-title");
-    const taskDescEle = document.querySelector("#task-desc");
+// //add to array
+//     const activeProject = document.querySelector("#add-list").getAttribute("active-project");
+//     const activeList = document.querySelector("#add-task").getAttribute("active-list");
+//     const taskNameEle = document.querySelector("#task-title");
+//     const taskDescEle = document.querySelector("#task-desc");
 
-    const newTaskName = taskNameEle.value;
-    const newTaskDesc = taskDescEle.value;;
+//     const newTaskName = taskNameEle.value;
+//     const newTaskDesc = taskDescEle.value;;
 
-    const listParentProject = ProjectManager.getAllProjects()[activeProject];
-    const taskParentList = listParentProject.getLists()[activeList];
+//     const listParentProject = ProjectManager.getAllProjects()[activeProject];
+//     const taskParentList = listParentProject.getLists()[activeList];
 
-    taskParentList.createTask(newTaskName, newTaskDesc);
-    populateNewtask(taskParentList.getAllTasks().length -1, newTaskName, newTaskDesc);
-    const taskDialog = document.querySelector("#task-dialog");
-    taskDialog.close();
-}
+//     taskParentList.createTask(newTaskName, newTaskDesc);
+//     populateNewtask(taskParentList.getAllTasks().length -1, newTaskName, newTaskDesc);
+//     const taskDialog = document.querySelector("#task-dialog");
+//     taskDialog.close();
+// }
--- src/scripts/populateDOM.js
+++ src/scripts/populateDOM.js
@@ -1,26 +1,6 @@
 // import { Project } from "./projectHandler";
 import data from "./defaultContent.json";
- function insertProjects(projectManager)
-{
-    const data = JSON.parse(data);
-    
-    data["projects"].forEach(project => {
-        projectManager.addProject(project["name"]);
-    });
 
-}
-
-function populateDOM(projects)
-{
-    
-    for(let i = 0; i < projects.length; ++i)
-    {
-     populateNewProject(projects[i], i);
-    }
-    // console.log(projects);
-    // console.log("These are lists:", projects[0].getLists());
-    populateNavigatorList(projects[0].getLists());
-}
 function populateNewProject(project, index)
 {
     const projectBar = document.querySelector("#all-projects");
@@ -30,34 +10,13 @@ function populateNewProject(project, index)
     pbtn.textContent = project.getProjectName();
     
     pli.setAttribute('project-index', index);
-    pli.addEventListener("click", ()=>
-        {populateNavigatorList(project.getLists(), index)})
+    // pli.addEventListener("click", ()=>
+    //     {populateNavigatorList(project.getLists(), index)})
     pli.appendChild(pbtn);
     projectBar.insertBefore(pli, addProjectBtn);
 }
 
-function populateNavigatorList(list, projectIndex)
-{    
-   
-    const listNavigator = document.querySelector("#all-lists");
-    const addListBtn = document.querySelector("#add-list");
-    addListBtn.setAttribute("active-project", projectIndex);
-    listNavigator.replaceChildren(addListBtn);
-    addListBtn.setAttribute("active-project", projectIndex);
-    for(let i = 0; i<list.length; ++i)
-        {
-            populateNewList(i, list[i])
 
-        }
-    if(list.length !=0)
-            {
-    populateListTask(0, list[0].getAllTasks());
-            }
-    else
-    {
-        return;
-    }
-}
 
 function populateNewList(index, list)
 {
@@ -108,11 +67,11 @@ function populateNewtask(todoIndex, taskName, taskDesc)
     mainList.appendChild(tdd);
 
 }
-export default function generateFakeContent()
+
+export default function generateDefaultContent()
 {
-   
     return data;
 }
 
 
-export {populateDOM, populateNavigatorList, populateListTask, populateNewProject, populateNewList, populateNewtask};
+export { populateListTask, populateNewProject, populateNewList, populateNewtask};
