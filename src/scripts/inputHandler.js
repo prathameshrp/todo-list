3d2afc4acd0687ab9c3b93ba78d352c9418d06df
prathamesh <prathamesh.math@gmail.com>
2024-09-01 12:50:20 +0530

    Deleted unused script files

--- src/scripts/inputHandler.js
+++
@@ -1,7 +0,0 @@
-export default function promptUser()
-{
-    const t = prompt("Enter title: ", );
-    const d = prompt("Enter description: ", );
-    const date = new Date();
-    return {t, d, date};
-}
